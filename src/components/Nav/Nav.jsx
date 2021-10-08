6bb32bcbd53464c60744e34827ab04d7a699b68c
Slashchykau Andrei <slashchykau@gmail.com>
2021-10-08 21:46:47 +0300

    instal react rourter dom and create news modules

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,11 +2,11 @@ import s from './Nav.module.css';
 const Nav = () => {
   return (
     <nav className={s.navbar}>
-      <a className={`${s.item} ${s.active}`} href="###" alt="abrak">Profile</a>
-      <a className={s.item} href="###" alt="abrak">Messages</a>
-      <a className={s.item} href="###" alt="abrak">News</a>
-      <a className={s.item} href="###" alt="abrak">Music</a>
-      <a className={s.item} href="###" alt="abrak">Settings</a>
+      <a className={`${s.item} ${s.active}`} href="/profile" alt="/profile">Profile</a>
+      <a className={s.item} href="/dialogs" alt="/dialogs">Messages</a>
+      <a className={s.item} href="/news" alt="/news">News</a>
+      <a className={s.item} href="/music" alt="/music">Music</a>
+      <a className={s.item} href="/settings" alt="/settings">Settings</a>
     </nav>
   )
 }
