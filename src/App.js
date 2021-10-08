a9fe507d669b04827bbe6d4cd32b9bd98dabef7f
Slashchykau Andrei <slashchykau@gmail.com>
2021-10-08 22:12:12 +0300

    Lern NaveLink how create app without updating page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
@@ -8,22 +7,23 @@ import {BrowserRouter, Route} from "react-router-dom";
 import Settings from "./components/Settings/Settings";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
+
 const App = () => {
-  return (
-      <BrowserRouter>
-    <div className="app-wrapper">
-      <Header />
-      <Nav />
-        <div className="app-wrapper-content">
-            <Route path='/dialogs' component={Dialogs}/>
-            <Route path='/profile' component={Profile}/>
-          <Route path='/music' component={Music}/>
-          <Route path ='/news' component={News}/>
-          <Route path='/settings' component={Settings}/>
-        </div>
-    </div>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Nav/>
+                <div className="app-wrapper-content">
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,13 @@
 import s from './Nav.module.css';
+import {NavLink} from "react-router-dom";
 const Nav = () => {
   return (
     <nav className={s.navbar}>
-      <a className={`${s.item} ${s.active}`} href="/profile" alt="/profile">Profile</a>
-      <a className={s.item} href="/dialogs" alt="/dialogs">Messages</a>
-      <a className={s.item} href="/news" alt="/news">News</a>
-      <a className={s.item} href="/music" alt="/music">Music</a>
-      <a className={s.item} href="/settings" alt="/settings">Settings</a>
+      <NavLink className={`${s.item}`} to="/profile" alt="/profile" activeClassName={s.active}>Profile</NavLink>
+      <NavLink className={s.item} to="/dialogs" alt="/dialogs" activeClassName={s.active}>Messages</NavLink>
+      <NavLink className={s.item} to="/news" alt="/news" activeClassName={s.active}>News</NavLink>
+      <NavLink className={s.item} to="/music" alt="/music" activeClassName={s.active}>Music</NavLink>
+      <NavLink className={s.item} to="/settings" alt="/settings" activeClassName={s.active}>Settings</NavLink>
     </nav>
   )
 }
