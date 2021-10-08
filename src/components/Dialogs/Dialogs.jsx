b83994bbe9182334d6cf69b0f17411251ea94000
Slashchykau Andrei <slashchykau@gmail.com>
2021-10-08 22:41:43 +0300

    create new diarection for dialogs users

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,20 @@
 import s from './Dialogs.module.css'
-const Dialogs = (props) => {
+const Dialogs = () => {
     return (
-        <div>Dialogs </div>
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' +s.active}>Vasa</div>
+                <div className={s.dialog}>Sveta</div>
+                <div className={s.dialog}>Galya</div>
+                <div className={s.dialog}>Arnest</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>hello!</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>I am fine thanks!</div>
+            </div>
+        </div>
     )
 }
 export default  Dialogs;
