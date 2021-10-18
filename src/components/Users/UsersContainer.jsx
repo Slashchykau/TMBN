aaca87771cfa088e29de46f1f940595bdcb9438a
Slashchykau Andrei <slashchykau@gmail.com>
2021-10-19 01:00:47 +0300

    add pagination users and update axios function for getUsers

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import {connect} from "react-redux";
 import Users from "./Users";
-import {setUsersAC, toggleFollowAC} from "../../redux/users.reducer";
+import {setCurrentAC, setTotalAC, setUsersAC, toggleFollowAC} from "../../redux/users.reducer";
 
 const mapStateToProps = (state) => {
 
     return {
-        users: state.usersPage.users}
-
+        users: state.usersPage.users,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
+        pageSize: state.usersPage.pageSize
+    }
 }
 const mapDispatchToProps = (dispatch) => {
-return {
-    toggleFollow:(userId) => {
-        dispatch(toggleFollowAC(userId))
-    },
-    setUsers: (users) => {
-        dispatch(setUsersAC(users))
+    return {
+        toggleFollow: (userId) => {
+            dispatch(toggleFollowAC(userId))
+        },
+        setUsers: (users) => {
+            dispatch(setUsersAC(users))
+        },
+        setCurrent: (currentPage) => {
+            dispatch(setCurrentAC(currentPage))
+        },
+        setTotalCount: (totalUsersCount) => {
+            dispatch(setTotalAC(totalUsersCount))
+        }
+
+
     }
-
-
-}
 }
 
-const UsersContainer = connect(mapStateToProps,mapDispatchToProps) (Users)
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
 export default UsersContainer;
--- src/redux/users.reducer.js
+++ src/redux/users.reducer.js
@@ -1,5 +1,7 @@
 const TOGGLE_FOLLOW = 'TOGGLE-FOLLOW'
 const SET_USERS = 'SET-USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_TOTAL_COUNT = 'SET_TOTAL_COUNT'
 
 const initialUsersState = {
     users: [
@@ -7,7 +9,10 @@ const initialUsersState = {
         // {id: 2, followed: false, photoURL: 'https://images.pexels.com/photos/4890733/pexels-photo-4890733.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', fullName: 'Borshchou Victor', status: 'bad guy', location: {city: 'Minsk', country: 'Belarus'}},
         // {id: 3, followed: false, photoURL: 'https://images.pexels.com/photos/4890733/pexels-photo-4890733.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', fullName: 'Pozdnyak Gleb', status: 'better guy', location: {city: 'Kiev', country: 'Ukraine'}},
         // {id: 4, followed: true, photoURL: 'https://images.pexels.com/photos/4890733/pexels-photo-4890733.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', fullName: 'Kolosov Vitaliy', status: 'the best guy', location: {city: 'Moscow', country: 'Russia'}}
-    ]
+    ],
+    pageSize: 5,
+    totalUsersCount: 5,
+    currentPage: 3
 
 };
 
@@ -28,7 +33,19 @@ const usersReducer = (state = initialUsersState, action) => {
         case SET_USERS: {
             return {
                 ...state,
-                users: [...state.users, ...action.users]
+                users: action.users
+            }
+        }
+        case SET_CURRENT_PAGE : {
+            return {
+                ...state,
+                currentPage: action.currentPage
+            }
+        }
+        case SET_TOTAL_COUNT : {
+            return {
+                ...state,
+                totalUsersCount: action.totalUsersCount
             }
         }
         default: return state;
@@ -41,6 +58,12 @@ export const toggleFollowAC = (userId) => {
 }
 export const setUsersAC = (users) => {
     return { type: SET_USERS, users}
+}
+export const setCurrentAC = (currentPage) => {
+    return { type: SET_CURRENT_PAGE, currentPage}
+}
+export const setTotalAC = (totalUsersCount) => {
+    return { type: SET_TOTAL_COUNT, totalUsersCount}
 }
 
 export default usersReducer;
