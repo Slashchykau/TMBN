7d438a338dc8d855702d156ef36ebaddfbfdae7b
Slashchykau Andrei <slashchykau@gmail.com>
2021-10-18 22:11:44 +0300

    learn  life cycle methods, componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,15 +5,12 @@ import userAvatar from '../../assets/img/users/png-transparent-computer-icons-an
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-        if (this.props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                    this.props.setUsers([...response.data.items])
-                }
-            )}
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                this.props.setUsers([...response.data.items])
+            }
+        )
     }
-
 
     render() {
         return (
